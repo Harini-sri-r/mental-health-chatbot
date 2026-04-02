1010d9bb70227619fe3973b5b280f96f2202fe84
Harini sri R <harinibrindhasri@gmail.com>
2026-04-02 16:06:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -318,4 +318,5 @@ def dashboard():
     return render_template("mood.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
